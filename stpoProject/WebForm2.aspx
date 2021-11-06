a05c626c352301c38dfe451f7e25711928d2efc3
GIKIK7 <giikik@wp.pl>
2021-11-06 17:37:59 +0100

    only sign in form added

---
+++ stpoProject/WebForm2.aspx
@@ -0,0 +1,81 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="stpoProject.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">  
+    <title></title>  
+    <style type="text/css">  
+        .auto-style1 {  
+            width: 100%;  
+        }  
+    </style>  
+</head>  
+<body>  
+    <form id="form1" runat="server">  
+    <div>  
+      
+        <table class="auto-style1">  
+            <tr>  
+                <td colspan="6" style="text-align: center; vertical-align: top">  
+                    <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="XX-Large" Font-Underline="True" Text="Sign In"></asp:Label>  
+                </td>  
+            </tr>  
+            <tr>  
+                <td> </td>  
+                <td style="text-align: center">  
+                    <asp:Label ID="Label2" runat="server" Font-Size="X-Large" Text="Login:"></asp:Label>  
+                </td>  
+                <td style="text-align: center">  
+                    <asp:TextBox ID="TextBox1" runat="server" Font-Size="X-Large"></asp:TextBox>  
+                </td>  
+                <td> </td>  
+                <td> </td>  
+                <td> </td>  
+            </tr>  
+            <tr>  
+                <td> </td>  
+                <td style="text-align: center">  
+                    <asp:Label ID="Label3" runat="server" Font-Size="X-Large" Text="Password :"></asp:Label>  
+                </td>  
+                <td style="text-align: center">  
+                    <asp:TextBox ID="TextBox2" runat="server" Font-Size="X-Large"></asp:TextBox>  
+                </td>  
+                <td> </td>  
+                <td> </td>  
+                <td> </td>  
+            </tr>  
+            <tr>  
+                <td> </td>  
+                <td> </td>  
+                <td> </td>  
+                <td> </td>  
+                <td> </td>  
+                <td> </td>  
+            </tr>  
+            <tr>  
+                <td> </td>  
+                <td> </td>  
+                <td style="text-align: center">  
+                    <asp:Button ID="Button1" runat="server" BorderStyle="None" Font-Size="X-Large" OnClick="Button1_Click" Text="Log In" />  
+                </td>  
+                <td> </td>  
+                <td> </td>  
+                <td> </td>  
+            </tr>  
+            <tr>  
+                <td> </td>  
+                <td> </td>  
+                <td>  
+                    <asp:Label ID="Label4" runat="server" Font-Size="X-Large"></asp:Label>  
+                </td>  
+                <td> </td>  
+                <td> </td>  
+                <td> </td>  
+            </tr>  
+        </table>  
+      
+    </div>  
+    </form>  
+</body>  
+</html>  
---
+++ stpoProject/WebForm2.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace stpoProject
+{
+
+
+    public partial class WebForm2
+    {
+
+        /// <summary>
+        /// Kontrolka form1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Kontrolka Label1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Kontrolka Label2.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Kontrolka TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Kontrolka Label3.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Kontrolka TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Kontrolka Button1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Kontrolka Label4.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+    }
+}
