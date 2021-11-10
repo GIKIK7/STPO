b5d7e58948ac64b270c8aa2b9e9c609ce53a73fb
GIKIK7 <giikik@wp.pl>
2021-11-10 17:08:09 +0100

    DataList for Coaches search added

---
+++ stpoProject/CoachSearchForm.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace stpoProject
+{
+    public partial class CoachSearchForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        void funkcja()
+        {
+            Lbl_helper.Text = "hahah gowno";
+        }
+    }
+}
---
+++ stpoProject/CoachSearchForm.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generowany automatycznie>
+//     Ten kod został wygenerowany przez narzędzie.
+//
+//     Modyfikacje tego pliku mogą spowodować niewłaściwe zachowanie i zostaną utracone
+//     w przypadku ponownego wygenerowania kodu. 
+// </generowany automatycznie>
+//------------------------------------------------------------------------------
+
+namespace stpoProject
+{
+
+
+    public partial class CoachSearchForm
+    {
+
+        /// <summary>
+        /// Kontrolka form1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Kontrolka DataList1.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DataList DataList1;
+
+        /// <summary>
+        /// Kontrolka DataSource_coaches.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource DataSource_coaches;
+
+        /// <summary>
+        /// Kontrolka Lbl_helper.
+        /// </summary>
+        /// <remarks>
+        /// Pole generowane automatycznie.
+        /// By zmodyfikować deklaracje pola przenieś ją z pliku projektanta do pliku codebehind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Lbl_helper;
+    }
+}
